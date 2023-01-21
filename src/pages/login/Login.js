43a753b4aebdcb529bbbff5cae191b34decad53c
Sara Chen <rainbow801122@gmail.com>
2023-01-22 00:18:52 +0800

    feat : 新增登入頁面內容

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,37 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
+import "./Login.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, isLoading, error } = useLogin();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email);
-    console.log(e);
+    login(email, password);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div></div>
-      <label>
-        <span>email:</span>
-        <input
-          type="email"
-          onChange={e => setEmail(e.target.value)}
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input
-          type="password"
-          onChange={e => setPassword(e.target.value)}
-          value={password}
-        />
-      </label>
-      <button>Login</button>
-    </form>
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Social Tree</h1>
+          <p>享受人與人之間無距離的互動</p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <label>
+              <span>信箱：</span>
+              <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </label>
+            <label>
+              <span>密碼：</span>
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </label>
+            {!isLoading && <button>Login</button>}
+            {isLoading && <button disabled>Loading</button>}
+            {error && <p>{error}</p>}
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
