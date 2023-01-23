c5920be84d31eabc5980aae0bf9ba9620390f9bc
Sara Chen <rainbow801122@gmail.com>
2023-01-24 01:17:27 +0800

    style : 修改程式碼

--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useAuthStatus } from "./useAuthStatus";
 
 export const useRegister = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const { dispatch } = useAuthStatus();
+  const navigate = useNavigate();
 
   const register = async (email, password, userName) => {
     setIsLoading(true);
@@ -15,22 +19,26 @@ export const useRegister = () => {
         email,
         password
       );
-      console.log(response.user);
+
       if (!response) {
-        throw new Error("Could not complete register");
+        throw new Error("Register Unsuccess");
       }
       await updateProfile(auth.currentUser, {
         displayName: userName,
       });
 
+      //dispatch login action
+      dispatch({ type: "Login", status: response.user });
+
       setIsLoading(false);
       setError(null);
+
+      navigate("/");
     } catch (error) {
       console.log(error.message);
       setIsLoading(false);
       setError(error.message);
     }
   };
-
   return { isLoading, error, register };
 };
