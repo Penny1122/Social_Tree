1c2c5953eae40809a40973d27a050c54dff47837
Sara Chen <rainbow801122@gmail.com>
2023-01-28 00:12:20 +0800

    style : 整理程式碼

--- src/hooks/useRegister.js
+++ src/hooks/useRegister.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../utils/firebase";
