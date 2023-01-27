77b17c2effafbbcc0cb36b96e0fac3ae6711a0bc
Sara Chen <rainbow801122@gmail.com>
2023-01-28 00:15:09 +0800

    feat : 新增會員狀態context

---
+++ src/context/AuthStatusContext.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer, useEffect } from "react";
+import React from "react";
+import { auth } from "../utils/firebase";
+
+export const AuthStatusContext = createContext();
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case "Login":
+      return { ...state, user: action.status };
+    case "Logout":
+      return { ...state, user: null };
+    case "Auth_Ready":
+      return { ...state, authReady: true, user: action.status };
+
+    default:
+      return state;
+  }
+};
+
+export const AuthStatusContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authReady: false,
+  });
+
+  useEffect(() => {
+    const authStatus = auth.onAuthStateChanged((user) => {
+      dispatch({ type: "Auth_Ready", status: user });
+      authStatus();
+    });
+  }, []);
+
+  console.log(state);
+
+  return (
+    <AuthStatusContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthStatusContext.Provider>
+  );
+};
