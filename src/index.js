e023e9d758fd26d0e4546786341348655afc07a7
Sara Chen <rainbow801122@gmail.com>
2023-01-31 00:36:14 +0800

    feat : 使用AuthStatusContextProvider

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { AuthStatusContextProvider } from "./context/AuthStatusContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthStatusContextProvider>
+      <App />
+    </AuthStatusContextProvider>
   </React.StrictMode>
 );
