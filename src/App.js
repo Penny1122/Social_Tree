59dd5be819311bc4bba4b936b62c3f041556067f
Sara Chen <rainbow801122@gmail.com>
2023-02-01 17:57:48 +0800

    feat : 新增路徑

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuthStatus } from "./hooks/useAuthStatus";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Introduce from "./pages/introduce/Introduce";
+import Navbar from "./components/Navbar";
 
-const App = () => {};
+const App = () => {
+  const { authReady, user } = useAuthStatus();
+
+  return (
+    <div>
+      {authReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {user && <Route path="/" element={<Home />} />}
+            {!user && <Route path="/" element={<Introduce />} />}
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
+  );
+};
 
 export default App;
