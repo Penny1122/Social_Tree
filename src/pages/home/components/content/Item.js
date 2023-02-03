1e26e37276c7b7cdd09ffd33ddd14f952fcfc709
Sara Chen <rainbow801122@gmail.com>
2023-02-04 01:23:25 +0800

    feat : 串接留言功能

--- src/pages/home/components/content/Item.js
+++ src/pages/home/components/content/Item.js
@@ -4,75 +4,117 @@ import Like from "../../../../images/like.png";
 import LikeFilled from "../../../../images/like-filled.png";
 import { BiLoaderCircle } from "react-icons/bi";
 import { BiSend } from "react-icons/bi";
-import { useGetPost } from "../../../../hooks/useGetPost";
+import { useGetComment } from "../../../../hooks/useGetComment";
 import { useLikePost } from "../../../../hooks/useLikePost";
 import { useAddComment } from "../../../../hooks/useAddComment";
 import { useAuthStatus } from "../../../../hooks/useAuthStatus";
-// import Comment from "./Comment";
+import Comment from "./Comment";
 
-const Item = ({ id, note, image, author, likedBy, doc }) => {
+const Item = ({ id, note, image, author, likedBy, commentCount }) => {
+  const { comments, isLoading } = useGetComment(id);
   const { user } = useAuthStatus();
-  const { isLoading } = useGetPost();
   const { likePost, isLoadingLike } = useLikePost();
-  const { addComment, isLoadingAddComment } = useAddComment();
+  const { addComment } = useAddComment();
   const [commentContent, setCommentContent] = useState("");
+  const [commentCLoadingIcon, setCommentCLoadingIcon] = useState(false);
+  const [showComments, setShowComments] = useState(false);
 
   const isLiked = likedBy?.includes(user.uid);
 
   const handleLikePost = () => {
     likePost(id, isLiked);
   };
-  const handleSubmit = () => {
-    addComment(id, commentContent);
-    setCommentContent("");
+  const handleSubmitComment = () => {
+    if (commentContent) {
+      setCommentCLoadingIcon(true);
+      addComment(id, commentContent);
+      setCommentContent("");
+      setTimeout(() => {
+        setCommentCLoadingIcon(false);
+      }, 1000);
+      setShowComments(true);
+    }
+  };
+  const showAllComments = () => {
+    if (!showComments) {
+      setShowComments(true);
+    } else {
+      setShowComments(false);
+    }
   };
 
   return (
     <>
-      {!isLoading && (
-        <div className="item">
-          <div className="poster">
-            <img className="poster-photo" src={MemberIcon} alt="" />
-            <span className="poster-name">{author.displayName}</span>
-          </div>
-          <div className="item-photo">
-            <img src={image} alt="" onDoubleClick={handleLikePost} />
-            {isLoadingLike && (
-              <div className="heart">
-                <img src={LikeFilled} alt="" />
+      <div className="item">
+        <div className="poster">
+          <img className="poster-photo" src={MemberIcon} alt="" />
+          <span className="poster-name">{author.displayName}</span>
+        </div>
+        <div className="item-photo">
+          <img src={image} alt="" onDoubleClick={handleLikePost} />
+          {isLoadingLike && (
+            <div className="heart">
+              <img src={LikeFilled} alt="" />
+            </div>
+          )}
+          <div className="item-bottom">
+            <div className="like">
+              <img
+                src={isLiked ? LikeFilled : Like}
+                alt=""
+                onClick={handleLikePost}
+              />
+              <span>{likedBy ? likedBy.length : 0} Likes</span>
+            </div>
+            <div className="text">
+              <span className="poster-name">{author.displayName}</span>
+              <span className="poster-text">{note}</span>
+            </div>
+            {commentCount && (
+              <div className="checkAllComments" onClick={showAllComments}>
+                {!showComments ? "查看" : "隱藏"}全部
+                {!showComments ? ` ${commentCount} 則` : ""}留言
               </div>
             )}
-            <div className="item-bottom">
-              <div className="like">
-                <img
-                  src={isLiked ? LikeFilled : Like}
-                  alt=""
-                  onClick={handleLikePost}
+            {!isLoading && showComments && (
+              <div>
+                {comments.map((doc) => {
+                  const { content, author, createdAt } = doc;
+                  return (
+                    <Comment
+                      content={content}
+                      key={doc.id}
+                      author={author}
+                      createdAt={createdAt}
+                    />
+                  );
+                })}
+              </div>
+            )}
+            <div className="message">
+              <img className="poster-photo" src={MemberIcon} alt="" />
+              <input
+                placeholder="新增留言"
+                className="message-input"
+                value={commentContent}
+                onChange={(e) => setCommentContent(e.target.value)}
+                onKeyPress={(e) => {
+                  if (e.key === "Enter") {
+                    handleSubmitComment();
+                  }
+                }}
+              />
+              {!commentCLoadingIcon && (
+                <BiSend
+                  className="send-comment"
+                  onClick={handleSubmitComment}
                 />
-
-                <span>{likedBy.length || 0} Likes</span>
-              </div>
-              <div className="text">
-                <span className="poster-name">{author.displayName}</span>
-                <span className="poster-text">{note}</span>
-              </div>
-              <div className="message">
-                <img className="poster-photo" src={MemberIcon} alt="" />
-                <input
-                  placeholder="新增留言"
-                  className="message-input"
-                  value={commentContent}
-                  onChange={(e) => setCommentContent(e.target.value)}
-                />
-                {!isLoadingAddComment && (
-                  <BiSend className="send-comment" onClick={handleSubmit} />
-                )}
-                {isLoadingAddComment && <BiLoaderCircle className="loading" />}
-              </div>
+              )}
+              {commentCLoadingIcon && <BiLoaderCircle className="loading" />}
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
