ebfc20b09f93c928034a35a7ea9288d6c4506cd6
Sara Chen <rainbow801122@gmail.com>
2023-02-04 01:27:40 +0800

    feat : 新增route : /user-profile/:userId

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Introduce from "./pages/introduce/Introduce";
+import UserProfile from "./pages/user-profile/userProfile";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   const { authReady, user } = useAuthStatus();
-
+  console.log(user);
   return (
     <div>
       {authReady && (
         <BrowserRouter>
           <Navbar />
           <Routes>
+            <Route path="/user-profile/:userId" element={<UserProfile />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             {user && <Route path="/" element={<Home />} />}
