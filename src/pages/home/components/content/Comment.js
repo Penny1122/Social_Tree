c6801dddc38dd5a5e461ff48b21baff8bc2196de
Sara Chen <rainbow801122@gmail.com>
2023-02-17 23:12:13 +0800

    整理程式碼

--- src/pages/home/components/content/Comment.js
+++ src/pages/home/components/content/Comment.js
@@ -1,15 +1,6 @@
-import { useAddComment } from "../../../../hooks/useAddComment";
 import React, { useState } from "react";
-import MemberIcon from "../../../../images/02-6.jpg";
-import { BiSend } from "react-icons/bi";
-import { BiLoaderCircle } from "react-icons/bi";
 
 const Comment = ({ content, author, createdAt }) => {
-  //   const { addComment, isLoadingAddComment } = useAddComment();
-  //   const [commentContent, setCommentContent] = useState("");
-  //   const handleSubmit = () => {
-  //     addComment(id, commentContent);
-  //   };
   return (
     <div>
       <div className="comment">
--- src/pages/home/components/content/content.js
+++ src/pages/home/components/content/content.js
@@ -7,20 +7,7 @@ import { useGetPost } from "../../../../hooks/useGetPost";
 const Content = () => {
   const [content, setContent] = useState([]);
   const { posts, isLoading } = useGetPost();
-  // useEffect(() => {
-  //   getContent();
-  // }, []);
-  // const getContent = () => {
-  //   setContent(function (prevContent) {
-  //     return [
-  //       ...prevContent,
-  //       {
-  //         id: "test",
-  //         note: "test",
-  //       },
-  //     ];
-  //   });
-  // };
+
   return (
     <li className="content">
       <AddPost add={setContent} />
