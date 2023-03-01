f6dc76bebafb4b0ec5fc7129db7541a2af6784f4
Sara Chen <rainbow801122@gmail.com>
2023-03-02 01:57:36 +0800

    feat : 新增個人頁面的相簿及好友列表

--- src/pages/user-profile/components/albumAndFriend.js
+++ src/pages/user-profile/components/albumAndFriend.js
@@ -1,23 +1,62 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { friendList } from "../../../hooks/useFriend";
+import { Link, useParams } from "react-router-dom";
 import "./albumAndFriend.css";
 
 const AlbumAndFriend = ({ posts }) => {
+  const { userId } = useParams();
+  const { friendsList } = friendList();
   const [clickAlbum, setClickAlbum] = useState(true);
   const [clickFriend, setClickFriend] = useState(false);
 
+  const handleAlbum = () => {
+    setClickAlbum(true);
+    setClickFriend(false);
+  };
+  const handleFriend = () => {
+    setClickAlbum(false);
+    setClickFriend(true);
+  };
+  useEffect(() => {
+    setClickAlbum(true);
+    setClickFriend(false);
+  }, [userId]);
   return (
     <div className="albumAndFriend">
       <div className="title">
-        <span className={clickAlbum ? "click" : ""}>相簿</span>
-        <span className={clickFriend ? "click" : ""}>好友</span>
+        <span className={clickAlbum ? "click" : ""} onClick={handleAlbum}>
+          相簿
+        </span>
+        <span className={clickFriend ? "click" : ""} onClick={handleFriend}>
+          好友
+        </span>
       </div>
-      <div className="photo">
-        {posts.map((doc) => {
-          const { id, imageURL } = doc;
-          return <img src={imageURL} key={id} />;
-        })}
-      </div>
+      {clickAlbum && (
+        <div className="photo">
+          {posts.map((doc) => {
+            const { id, imageURL } = doc;
+            return <img src={imageURL} key={id} />;
+          })}
+        </div>
+      )}
+      {clickFriend && (
+        <div className="friend">
+          {friendsList.map((doc) => {
+            const { id, photoURL, displayName } = doc;
+            return (
+              <Link to={`/user-profile/${id}`} key={id}>
+                <span className="friend-item">
+                  <div>
+                    <img src={photoURL} />
+                    <span className="name">{displayName}</span>
+                  </div>
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
