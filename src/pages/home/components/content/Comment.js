41f4da14e8b1878532665bb03d2e987d9e1bf439
Sara Chen <rainbow801122@gmail.com>
2023-03-02 01:59:07 +0800

    feat : 增加留言者大頭貼

--- src/pages/home/components/content/Comment.js
+++ src/pages/home/components/content/Comment.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Comment = ({ content, author, createdAt }) => {
   return (
     <div>
       <div className="comment">
-        <span className="poster-name">{author.displayName}</span>
+        <Link to={`/user-profile/${author.uid}`}>
+          <span className="poster-name">{author.displayName}</span>
+        </Link>
         <span className="poster-text">{content}</span>
       </div>
     </div>
