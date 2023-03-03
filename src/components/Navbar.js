ecb4b13e171121d0adef0a30b53aab4a81138687
Sara Chen <rainbow801122@gmail.com>
2023-03-03 23:32:46 +0800

    feat : 新增chatRoom icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useGetNoticeLength } from "../hooks/useGetNotice";
 import { useSearchUser } from "../hooks/useFriend";
 import { BiSearch } from "react-icons/bi";
 import { CgTrees } from "react-icons/cg";
-import { HiBell } from "react-icons/hi";
+import { HiBell, HiChat } from "react-icons/hi";
 import { FaUserFriends } from "react-icons/fa";
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import { MdPostAdd } from "react-icons/md";
 import { useAuthStatus } from "../hooks/useAuthStatus";
 import "./Navbar.css";
 import Notice from "./Notice";
@@ -25,6 +24,7 @@ const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthStatus();
   const { notice } = useGetNoticeLength();
+  const navigate = useNavigate();
 
   const toggleList = () => {
     setShowFriendList(false);
@@ -44,142 +44,151 @@ const Navbar = () => {
     setShowFriendList(false);
     setShowAddPost(!showAddPost);
   };
+  const handleLink = () => {
+    navigate("/chatroom");
+  };
   useEffect(() => {
     if (showSearch) {
       searchRef.current.focus();
     }
   }, [showSearch]);
   return (
-    <div className="navbar">
-      <ul className="navbar-ul">
-        <div className="title-aTag">
-          <Link to="/">
-            <li className="title">
-              <CgTrees className="logo" />
-              <span className="title-name">Social</span>
-              <span className="title-name">Tree</span>
-            </li>
-          </Link>
-          {showIcon && (
-            <div
-              className="circle-box RWDIcon"
-              onClick={() => {
-                setShowIcon(false);
-                setShowSearch(true);
-              }}
-            >
-              <BiSearch className="notice" />
-            </div>
-          )}
-          {user && (
-            <li className="search-bar-box">
-              {!showSearch && (
-                <>
-                  <input
-                    className="search-bar"
-                    type="text"
-                    placeholder="Search User..."
-                    onClick={() => setShowSearch(true)}
-                  />
-                  <BiSearch className="search-bar-icon" />
-                </>
-              )}
-
-              {showSearch && (
-                <div
-                  className="search-user-list"
-                  style={
-                    userList && {
-                      boxShadow: "rgb(149 157 165 / 20%) 0px 8px 24px",
-                    }
-                  }
-                >
-                  <div className="search-bar2-box">
-                    <AiOutlineArrowLeft
-                      className="arrow"
-                      onClick={() => {
-                        setShowSearch(false);
-                        setShowIcon(true);
-                      }}
-                    />
+    <>
+      <div div className="navbar-fix"></div>
+      <div className="navbar">
+        <ul className="navbar-ul">
+          <div className="title-aTag">
+            <Link to="/">
+              <li className="title">
+                <CgTrees className="logo" />
+                <span className="title-name">Social</span>
+                <span className="title-name">Tree</span>
+              </li>
+            </Link>
+            {showIcon && (
+              <div
+                className="circle-box RWDIcon"
+                onClick={() => {
+                  setShowIcon(false);
+                  setShowSearch(true);
+                }}
+              >
+                <BiSearch className="notice" />
+              </div>
+            )}
+            {user && (
+              <li className="search-bar-box">
+                {!showSearch && (
+                  <>
                     <input
-                      ref={searchRef}
-                      className="search-bar2"
+                      className="search-bar"
                       type="text"
                       placeholder="Search User..."
-                      value={queryUser}
-                      onChange={handleSearchUser}
+                      onClick={() => setShowSearch(true)}
                     />
-                  </div>
-                  {userList && (
-                    <>
-                      {userList.map((user) => (
-                        <Link
-                          to={`/user-profile/${user.id}`}
-                          key={user.id}
-                          className="search-user"
-                          onClick={() => {
-                            setShowSearch(false);
-                            setShowIcon(true);
-                          }}
-                        >
-                          <div
+                    <BiSearch className="search-bar-icon" />
+                  </>
+                )}
+
+                {showSearch && (
+                  <div
+                    className="search-user-list"
+                    style={
+                      userList && {
+                        boxShadow: "rgb(149 157 165 / 20%) 0px 8px 24px",
+                      }
+                    }
+                  >
+                    <div className="search-bar2-box">
+                      <AiOutlineArrowLeft
+                        className="arrow"
+                        onClick={() => {
+                          setShowSearch(false);
+                          setShowIcon(true);
+                        }}
+                      />
+                      <input
+                        ref={searchRef}
+                        className="search-bar2"
+                        type="text"
+                        placeholder="Search User..."
+                        value={queryUser}
+                        onChange={handleSearchUser}
+                      />
+                    </div>
+                    {userList && (
+                      <>
+                        {userList.map((user) => (
+                          <Link
+                            to={`/user-profile/${user.id}`}
+                            key={user.id}
                             className="search-user"
                             onClick={() => {
                               setShowSearch(false);
-                              setQueryUser("");
+                              setShowIcon(true);
                             }}
                           >
-                            <img className="poster-photo" src={user.photoURL} />
-                            <span>{user.displayName}</span>
-                          </div>
-                        </Link>
-                      ))}
-                    </>
-                  )}
-                </div>
-              )}
+                            <div
+                              className="search-user"
+                              onClick={() => {
+                                setShowSearch(false);
+                                setQueryUser("");
+                              }}
+                            >
+                              <img
+                                className="poster-photo"
+                                src={user.photoURL}
+                              />
+                              <span>{user.displayName}</span>
+                            </div>
+                          </Link>
+                        ))}
+                      </>
+                    )}
+                  </div>
+                )}
+              </li>
+            )}
+          </div>
+
+          {!user && (
+            <li className="btn">
+              <Link to="/login">註冊 / 登入</Link>
             </li>
           )}
-        </div>
+          {user && (
+            <>
+              <div className="circle-box" onClick={handleLink}>
+                <HiChat className="notice" />
+              </div>
+              <div className="circle-box RWDIcon" onClick={toggleFriendList}>
+                <FaUserFriends className="notice" />
+                {showFriendList && (
+                  <li className="friend">
+                    <FriendsInvited />
+                  </li>
+                )}
+              </div>
+              <div className="circle-box" onClick={toggleList}>
+                <HiBell className="notice" />
+                {notice.length > 0 && (
+                  <div className="notice-count">{notice.length}</div>
+                )}
+                {showList && user && <Notice />}
+              </div>
+              <Link to={`/user-profile/${user.uid}`}>
+                <img className="poster-photo" src={user.photoURL} alt="" />
+              </Link>
 
-        {!user && (
-          <li className="btn">
-            <Link to="/login">註冊 / 登入</Link>
-          </li>
-        )}
-        {user && (
-          <>
-            <div className="circle-box" onClick={handleShowAddPost}>
-              <MdPostAdd className="notice" />
-            </div>
-            <div className="circle-box RWDIcon" onClick={toggleFriendList}>
-              <FaUserFriends className="notice" />
-              {showFriendList && (
-                <li className="friend">
-                  <FriendsInvited />
-                </li>
-              )}
-            </div>
-            <div className="circle-box" onClick={toggleList}>
-              <HiBell className="notice" />
-              {notice.length > 0 && (
-                <div className="notice-count">{notice.length}</div>
-              )}
-              {showList && user && <Notice />}
-            </div>
-            <Link to={`/user-profile/${user.uid}`}>
-              <img className="poster-photo" src={user.photoURL} alt="" />
-            </Link>
-
-            <li className="hello">Hello, {user.displayName}</li>
-            <li className="btn" onClick={logout}>
-              登出
-            </li>
-          </>
-        )}
-      </ul>
-    </div>
+              <li className="hello">Hello, {user.displayName}</li>
+              <li className="btn" onClick={logout}>
+                登出
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
