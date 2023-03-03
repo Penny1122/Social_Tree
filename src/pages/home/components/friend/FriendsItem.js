8606514d6efadd01a3ca63de8ae7bd91c42498ba
Sara Chen <rainbow801122@gmail.com>
2023-03-03 23:33:46 +0800

    feat : 新增顯示有未讀訊息及導向聊天室

--- src/pages/home/components/friend/FriendsItem.js
+++ src/pages/home/components/friend/FriendsItem.js
@@ -1,17 +1,35 @@
 import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { useGetPost } from "../../../../hooks/useGetPost";
-import { HiUserAdd } from "react-icons/hi";
+import { useChatStatus } from "../../../../hooks/useChat";
+import { useChat } from "../../../../hooks/useChat";
+import { useNavigate } from "react-router-dom";
+import { chatNotice } from "../../../../hooks/useChat";
+import { BsDot } from "react-icons/bs";
 
 const FriendsItem = ({ uid, displayName, photoURL, invitedAt }) => {
+  const { dispatch } = useChatStatus();
+  const { CreateGroupFromFriendList } = useChat();
+  const { notice } = chatNotice(uid);
+  const navigate = useNavigate();
+  const handleLinkToChatRoom = () => {
+    CreateGroupFromFriendList({ uid, photoURL, displayName });
+    dispatch({
+      type: "Change_user",
+      payload: {
+        uid,
+        photoURL,
+        displayName,
+      },
+    });
+    navigate("/chatRoom");
+  };
   return (
     <>
-      <div className="user">
-        <Link>
+      <div className="user" onClick={handleLinkToChatRoom}>
+        <div className="link">
           <img className="user-photo" src={photoURL} />
           <span className="user-name">{displayName}</span>
-        </Link>
+          {notice && <BsDot className="read" />}
+        </div>
       </div>
     </>
   );
