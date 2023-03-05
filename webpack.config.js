88b35599c31ad044d0ad0797c288ca4167b76a2d
Sara Chen <rainbow801122@gmail.com>
2023-03-05 19:24:58 +0800

    整理程式碼

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   //development
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
